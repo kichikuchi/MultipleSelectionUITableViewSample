3601bb63ae7461dfd0a46bf573a4706b3dbbd90b
kichikuchi <kikuchi0111@gmail.com>
2016-01-05 22:37:16 +0900

    Initial Commit

---
+++ MultipleSelectionUITableViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipleSelectionUITableViewSample
+//
+//  Created by 菊池和紀 on 2016/01/05.
+//  Copyright © 2016年 kazunori kikuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
